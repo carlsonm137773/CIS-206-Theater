fcf26d447ffa083b4fd01b705979226200805e66
matthew carlson <0196232@workbench.cis>
2019-03-24 17:23:42 -0700

    update

---
+++ Main.java
@@ -0,0 +1,45 @@
+/*
+* Project: Theater
+* File: Main.java
+* Description: Prompt the user to pick either a seat or a price. Mark that seat as sold on the array by setting the valuse to 0
+* Developer: Matthew Carlson
+* Email: carlsonm137773@student.vvc.edu
+* Date: 03/24/2019
+*/
+
+
+public class Main
+{
+	/*
+	* Prompt the user for a valid number Between min and max
+	* @peram promt the string outputed to the user
+	* @peram min the minimum value
+	* @peram max the maximum value
+	* @return the value input by the user
+	*/
+	public static int get_int(String prompt, int min, int max)
+	{
+		int number = 0;
+
+		return number;
+	}
+	
+   public static void main(String[] args)
+   {
+   	int[][] theater = {
+									{ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 },
+									{ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 },
+									{ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 },
+									{ 10, 10, 20, 20, 20, 20, 20, 20, 10, 10 },
+									{ 10, 10, 20, 20, 20, 20, 20, 20, 10, 10 },
+									{ 10, 10, 20, 20, 20, 20, 20, 20, 10, 10 },
+									{ 20, 20, 30, 30, 40, 40, 30, 30, 20, 20 },
+									{ 20, 30, 30, 40, 50, 50, 40, 30, 30, 20 },
+									{ 30, 40, 50, 50, 50, 50, 50, 50, 40, 30 },
+								};
+		//
+
+
+   }
+}
+
